4068460c8aa76a7f2f06dc67a43de32ec7ed450c
lucas <lucas@localhost>
2013-10-06 20:36:49 -0300

    El dictado escribe bien las mayusculas

--- src/main/java/org/squadra/atenea/actions/Dictate.java
+++ src/main/java/org/squadra/atenea/actions/Dictate.java
@@ -7,7 +7,6 @@ public class Dictate extends PreloadAction {
 
 	@Override
 	public void execute() {
-		// MainGUI.getInstance().mainButtonMouseClicked();
 		typeString(param);
 	}
 
@@ -17,17 +16,17 @@ public class Dictate extends PreloadAction {
 			byte[] bytes = s.getBytes();
 			for (byte b : bytes) {
 				int code = b;
-				// keycode only handles [A-Z] (which is ASCII decimal [65-90])
-				if (code > 64 && code < 91) 
+				// Si la letra es mayuscula, apreto Shift
+				if (code > 64 && code < 91)
 					robik.keyPress(KeyEvent.VK_SHIFT);
 				if (code > 96 && code < 123) 
-					code -= 32;
+					code -= 32;		
 				
-				System.out.print(code);
 				robik.delay(40);
 				robik.keyPress(code);
 				robik.keyRelease(code);
-				if (code+32 > 64 && code+32 < 91)
+				// Si la letra es mayuscula, suelto Shift
+				if (code > 64 && code < 91)
 					robik.keyRelease(KeyEvent.VK_SHIFT);
 			}
 		} catch (Exception e) {}
